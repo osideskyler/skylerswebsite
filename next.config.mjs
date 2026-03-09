6a555343945fc9498a80ad7be0f0e9fdeda0ad0d
osideskyler <osidesky@byu.edu>
2026-03-08 20:24:47 -0600

    config for github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+const repoName = "skylerswebsite";
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+
 const nextConfig = {
   output: "export",
+  ...(isGithubPages
+    ? {
+        basePath: `/${repoName}`,
+        assetPrefix: `/${repoName}/`,
+      }
+    : {}),
   images: {
     unoptimized: true,
   },
